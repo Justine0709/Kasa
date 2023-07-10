559ea29922bab4619e8ee0a98f1c3820268401b6
Justine Delsoir <justine.delsoir0709@gmail.com>
2023-07-10 11:27:17 +0200

    modif Accomodation.js

--- src/pages/Accommodation.js
+++ src/pages/Accommodation.js
@@ -1,6 +1,7 @@
-
-
-
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { AccommodationsList } from '../datas/AccommodationsLIst';
+import Header from '../components/Header';
 
 
 
